79b154e2d9d22d4e2f56e9d9cb6c34ca18f400c9
William Y Wong <wwong1987@gmail.com>
2019-03-17 20:51:35 -0700

    fixed nav bar to match wireframe

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
+import './App.css';
 
 class App extends Component {
   render() {
