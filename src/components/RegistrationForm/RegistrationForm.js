5fa3e8d6c9a55da7e88facc9fb1f4de8b9bdb9c7
William Y Wong <wwong1987@gmail.com>
2019-03-18 09:15:16 -0700

    added skeleton for registration form

---
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Button, Required, Input } from '../Utils/Utils'
+
+export default class RegistrationForm extends Component {
+  render() {
+    return (
+      <form className="RegistrationForm" onSubmit={this.handleSubmit}>
+        <div className="full_name">
+          <label htmlFor="RegistrationForm_full_name">
+            Full name <Required />
+          </label>
+          <Input
+            name="full_name"
+            type="text"
+            id="RegistrationForm_full_name"
+            required>
+           </Input>
+        </div>
+
+        <div className="user_name">
+          <label htmlFor="RegistrationForm_user_name">
+            User name <Required />
+          </label>
+          <Input
+            name='user_name'
+            type='text'
+            id='RegistrationForm__user_name'
+            required>
+          </Input>
+        </div>
+
+        <div className="password">
+          <label htmlFor="RegistrationForm_password">
+            Password <Required />
+          </label>
+          <Input
+            name='password'
+            type='password'
+            id='RegistrationForm__password'
+            required>
+          </Input>
+        </div>
+        <Button type='submit'>
+            Register
+        </Button>
+      </form>
+    );
+  }
+}
---
+++ src/components/Utils/Utils.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Utils.css'
+
+export function Button ({ className, ...props }) {
+    return <button className={['Button', className].join(' ')} {...props} />
+}
+
+export function Input({ className, ...props }) {
+    return (
+      <input className={['Input', className].join(' ')} {...props} />
+    )
+}
+  
+export function Required({ className, ...props }) {
+    return (
+        <span className={['Required', className].join(' ')} {...props}>
+        &#42;
+        </span>
+    )
+}
