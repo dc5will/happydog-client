18b1c7f0078e710cc724d332751e63ee79794bec
William Y Wong <wwong1987@gmail.com>
2019-03-20 19:05:53 -0700

    added checklist reset functionality

--- src/components/Checklist/Checklist.js
+++ src/components/Checklist/Checklist.js
@@ -1,71 +1,51 @@
 import React, { Component } from 'react';
+import CheckBox from '../CheckBox/CheckBox';
 import './Checklist.css'
 
-const Checkbox = props => (
-    <input type='checkbox' {...props} />
-)
 
 export default class Checklist extends Component {
-    state = { checked: false }
-
-    handleCheckboxChange = event => 
-        this.setState({
-            checked: event.target.checked
+    constructor(props) {
+        super(props)
+        this.state = {
+          tasks: [
+            {id: 1, value: "Breakfast", isChecked: false},
+            {id: 2, value: "Lunch", isChecked: false},
+            {id: 3, value: "Dinner", isChecked: false}
+          ]
+        }
+      }
+      
+      handleAllChecked = (event) => {
+        let tasks = this.state.tasks
+        tasks.forEach(task => task.isChecked = event.target.checked) 
+        this.setState({tasks: tasks})
+      }
+    
+      handleCheckedElement = (event) => {
+        let tasks = this.state.tasks
+        tasks.forEach(task => {
+           if (task.value === event.target.value)
+              task.isChecked =  event.target.checked
         })
-
-    render() {
+        this.setState({tasks: tasks})
+      }
+    
+      render() {
         return (
-            <React.Fragment>
-            <div>
-                <header>
-                    <h4>Has your dog eaten?</h4>
-                </header>
-                <label>
-                    <Checkbox
-                        checked={this.state.checked}
-                        onChange={this.handleCheckboxChange}
-                        />
-                        <span>Breakfast</span>
-                </label><br/>
-                <label>
-                    <Checkbox
-                        // checked={this.state.checked}
-                        // onChange={this.handleCheckboxChange}
-                        />
-                        <span>Lunch</span>
-                </label><br/>
-                <label>
-                    <Checkbox
-                        // checked={this.state.checked}
-                        // onChange={this.handleCheckboxChange}
-                        />
-                        <span>Dinner</span>
-                </label>
-            </div>
-            <header>
-            <h4>Have you walked your dog?</h4>
-                </header>
-                <label>
-                    <Checkbox
-                        />
-                        <span>Morning</span>
-                </label><br/>
-                <label>
-                    <Checkbox
-                        // checked={this.state.checked}
-                        // onChange={this.handleCheckboxChange}
-                        />
-                        <span>Afternoon</span>
-                </label><br/>
-                <label>
-                    <Checkbox
-                        // checked={this.state.checked}
-                        // onChange={this.handleCheckboxChange}
-                        />
-                        <span>Night time</span>
-                </label><br/>
-                <button className='resetButton'>Reset Daily Checklist</button>
-                </React.Fragment>
+          <div className="Checklist">
+          <header>
+            <h3>Have they eaten?</h3>
+        </header>
+            <ul>
+            {
+              this.state.tasks.map((tasks, index) => {
+                return (<CheckBox key={index} handleCheckedElement={this.handleCheckedElement}  {...tasks} />)
+              })
+            }
+            </ul>
+            <button className='resetButton' onClick={this.handleAllChecked}>Survived the day</button>
+          </div>
         );
+      }
     }
-}
+    
