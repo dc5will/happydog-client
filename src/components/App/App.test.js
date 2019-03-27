557e6a5702afec4e9798f42539e263612937985c
William Y Wong <wwong1987@gmail.com>
2019-03-27 15:48:17 -0700

    added initial smoke and snapshot test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
+import App from "./App";
+
+// smoke test
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
 
 
-it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(
-        <BrowserRouter>
-            <App />  
-        </BrowserRouter>,
-        div
+// snapshot test
+it("renders the UI as expected", () => {
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     )
-    ReactDOM.unmountComponentAtNode(div)
-})
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
