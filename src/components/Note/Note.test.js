ca024b20c34bd8affeea6085e61625f970314eb0
William Y Wong <wwong1987@gmail.com>
2019-03-27 20:20:52 -0700

    fixed snapshot tests and note.test.js

--- src/components/Note/Note.test.js
+++ src/components/Note/Note.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Login from "./Login";
+import Note from "./Note";
 // import renderer from "react-test-renderer";
 // import { MemoryRouter } from "react-router-dom";
 import Enzyme from "enzyme";
@@ -8,9 +8,9 @@ import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe(`LandingPage component`, () => {
+describe(`Note component`, () => {
   it("renders without crashing", () => {
-    shallow(<Login note={{name: test, id: 1}}/>);
+    shallow(<Note note={{name: test, id: 1}}/>);
   });
 
 //   it("renders the UI as expected", () => {
